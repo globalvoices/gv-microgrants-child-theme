cee23447ec12b3282f0bef5c1ffe82e47b6b63d1
jeremyclarke <jer@simianuprising.com>
2014-09-25 14:34:38 -0400

    update geo-mashup-info-window.php to load the version from parent theme.

--- geo-mashup-info-window.php
+++ geo-mashup-info-window.php
@@ -9,26 +9,4 @@
  * @package GeoMashup
  */
 
-// A potentially heavy-handed way to remove shortcode-like content
-add_filter( 'the_excerpt', array( 'GeoMashupQuery', 'strip_brackets' ) );
-
-?>
-<div class="locationinfo post-location-info">
-<?php if (have_posts()) : ?>
-
-	<?php while (have_posts()) : the_post();
-	
-		 gv_display_post_summary('', array(
-			 'hide_meta' => 1,
-		 ));
-	
-	endwhile; ?>
-
-<?php else : ?>
-
-	<h2 class="center">Not Found</h2>
-	<p class="center">Sorry, but you are looking for something that isn't here.</p>
-
-<?php endif; ?>
-
-</div>
+include(get_template_directory() . "/geo-mashup-info-window.php");
