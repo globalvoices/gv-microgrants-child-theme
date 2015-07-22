6e2f3a3e462b88787e36e6f1a601b2ea2a8e8199
jeremyclarke <jer@simianuprising.com>
2015-07-22 10:56:33 -0400

    functions.php - update rv logo in gv_og_image_default and disable gv_og_image (complete override of og:image)

--- functions.php
+++ functions.php
@@ -846,7 +846,7 @@ if (isset($gv) AND is_object($gv)) :
 	 * @return string desired icon
 	 */
 	function gvadvocacy_theme_gv_og_image_default($icon) {
-		return gv_get_dir('theme_images') ."rv-logo-square-600.png";
+		return gv_get_dir('theme_images') ."rv-logo-facebook-og-1200x631.png";
 	}
 	add_filter('gv_og_image_default', 'gvadvocacy_theme_gv_og_image_default');
 	
@@ -859,7 +859,7 @@ if (isset($gv) AND is_object($gv)) :
 	function gvadvocacy_theme_gv_og_image($icon) {
 		return gv_get_dir('theme_images') ."rv-logo-square-600.png";
 	}
-	add_filter('gv_og_image', 'gvadvocacy_theme_gv_og_image');
+//	add_filter('gv_og_image', 'gvadvocacy_theme_gv_og_image');
 	
 	/**
 	 * Define the hierarchical structure of the taxonomy by its parents
